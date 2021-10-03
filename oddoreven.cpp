0701ac0319e754f3808b249e2e6e7c65de209385
Alain Eleazar Eusebio <jewen7@yahoo.com>
2021-10-03 20:11:46 +0800

    oddoreven.cpp

---
+++ oddoreven.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int num, dnum;
+    int i = 0;
+
+    cout << "Enter your range of numbers ";
+    cin >> num;
+    
+    while (i < num)
+    {
+        cout << "Please Enter The Number : ";
+        cin >> dnum;
+        if(num %2 == 0)
+        {
+            cout << "Your Number is even" << endl;
+        }
+        else 
+        {
+            cout << "Your Number is odd" << endl;
+        }
+        i++;
+    }
+    return 0;
+}
